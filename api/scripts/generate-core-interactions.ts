fdd038978264943941824db5c0a4a53908d84edc
Bart <bartolomej.kozorog@gmail.com>
2024-09-06 01:01:37 +0200

    fixed various issues

--- api/scripts/generate-core-interactions.ts
+++ api/scripts/generate-core-interactions.ts
@@ -1,10 +1,12 @@
 import simpleGit, { SimpleGit } from 'simple-git';
 import glob from 'glob';
 import path from 'path';
-import fs from 'fs/promises';
+import fsPromises from 'fs/promises';
+import fs from "fs";
 import { spawn } from "node:child_process"
 
-// Define the Repository type
+const cleanupTempFolders = false;
+
 type Repository = {
     gitUrl: string;
     interactionPaths: string[];
@@ -13,17 +15,27 @@ type Repository = {
 // Define the repositories array
 const repos: Repository[] = [
     {
+        gitUrl: "https://github.com/onflow/flow-nft",
+        interactionPaths: [
+            "transactions/*",
+        ],
+    },
+    {
+        gitUrl: "https://github.com/onflow/flow-evm-bridge",
+        interactionPaths: [
+            "cadence/transactions/bridge/nft/*",
+            "cadence/transactions/bridge/onboarding/*",
+            "cadence/transactions/bridge/tokens/*",
+            "cadence/transactions/evm/*",
+            "cadence/transactions/flow-token/*",
+        ],
+    },
+    {
         gitUrl: "https://github.com/onflow/flow-core-contracts",
         interactionPaths: [
             "transactions/accounts/*",
             "transactions/flowToken/*",
             "transactions/randomBeaconHistory/*",
-        ],
-    },
-    {
-        gitUrl: "https://github.com/onflow/flow-nft",
-        interactionPaths: [
-            "transactions/*",
         ],
     },
     {
@@ -35,33 +47,53 @@ const repos: Repository[] = [
     },
 ];
 
-async function processCadenceFile(filePath: string): Promise<void> {
-    console.log(`Processing: ${filePath}`);
-
+async function processCadenceFile(projectPath: string, cdcFilePath: string): Promise<void> {
     try {
-        const flixTemplate = await flixGenerate(filePath);
-        // TODO: Update and write this template to `/templates` folder
-        console.log({ flixTemplate });
+        const projectName = projectPath.split("/").at(-1)!;
+        const flixFileName = `${cdcFilePath.split("/").at(-1)!.replace(".cdc", "")}.template.json`;
+        const flixTemplate = await flixGenerate(projectPath, cdcFilePath);
+        console.log(`Succeed processing ${cdcFilePath}:`)
+        const flixSubFolderPath = path.join(
+            process.cwd(),
+            "templates",
+            projectName,
+        );
+        const flixFilePath = path.join(
+            flixSubFolderPath,
+            flixFileName
+        );
+        if (!fs.existsSync(flixSubFolderPath)) {
+            await fsPromises.mkdir(flixSubFolderPath);
+        }
+        await fsPromises.writeFile(flixFilePath, flixTemplate);
     } catch (error) {
+        console.error(`Failed processing ${cdcFilePath}:`)
         console.error(error);
     }
 }
 
-function flixGenerate(sourceFilePath: string): Promise<any> {
+function flixGenerate(projectPath: string, sourceFilePath: string): Promise<any> {
     const outFilePath = `${sourceFilePath}.flix.json`;
     return new Promise((resolve, reject) => {
-        // Expects the latest Cadence preview flow-cli bin in the current directory
-        // See: https://github.com/onflow/flow-cli/releases
-        const flowProcess = spawn(path.join(__dirname, "./flow-cli"), ["flix", "generate", sourceFilePath, "--save", outFilePath])
+        const flowProcess = spawn(
+            // The Flow CLI binary here must be manually built from source
+            // and include this change: https://github.com/onflow/flixkit-go/pull/78
+            path.join(__dirname, "./flow-cli"),
+            ["flix", "generate", sourceFilePath, "--save", outFilePath],
+            {
+                cwd: projectPath
+            }
+        )
 
-        flowProcess.stderr.pipe(process.stderr);
+        let error = "";
+        flowProcess.stderr.on("data", data => error += data.toString());
 
         flowProcess.once("exit", async (code) => {
             if (code != null && code === 0) {
-                const fileContent = await fs.readFile(outFilePath, "utf-8");
+                const fileContent = await fsPromises.readFile(outFilePath, "utf-8");
                 resolve(fileContent.toString());
             } else {
-                reject(`Failed to process ${sourceFilePath} with code: ${code}`)
+                reject(error);
             }
         });
     })
@@ -69,30 +101,37 @@ function flixGenerate(sourceFilePath: string): Promise<any> {
 
 async function processRepositories(repos: Repository[]): Promise<void> {
     for (const repo of repos) {
-        const repoName = new URL(repo.gitUrl).pathname.split('/').slice(-2).join('-');
+        const repoName = new URL(repo.gitUrl).pathname.split('/').at(-1)!;
         const tempDir = path.join(process.cwd(), 'temp', repoName);
 
-        console.log(`Cloning ${repo.gitUrl} into ${tempDir}...`);
-        await cloneRepository(repo.gitUrl, tempDir);
+        if (!fs.existsSync(tempDir)) {
+            console.log(`Cloning ${repo.gitUrl} into ${tempDir}...`);
+            await cloneRepository(repo.gitUrl, tempDir);
+        }
 
         try {
             for (const interactionPath of repo.interactionPaths) {
                 const files = await findFiles(tempDir, interactionPath);
-                files
-                    .filter(file => file.endsWith(".cdc"))
-                    .forEach(file => processCadenceFile(file));
+                console.log(files.filter(file => file.endsWith(".cdc")))
+                await Promise.all(
+                    files
+                        .filter(file => file.endsWith(".cdc"))
+                        .map(file => processCadenceFile(tempDir, file))
+                );
             }
         } catch (error) {
             console.error(`Error processing repository ${repo.gitUrl}:`, error);
         } finally {
-            await cleanupDirectory(tempDir);
+            if (cleanupTempFolders) {
+                await cleanupDirectory(tempDir);
+            }
         }
     }
 }
 
 async function cloneRepository(gitUrl: string, targetDir: string): Promise<void> {
     const git: SimpleGit = simpleGit();
-    await fs.mkdir(targetDir, { recursive: true });
+    await fsPromises.mkdir(targetDir, { recursive: true });
     await git.clone(gitUrl, targetDir);
 }
 
@@ -110,7 +149,7 @@ async function findFiles(basePath: string, pattern: string): Promise<string[]> {
 
 async function cleanupDirectory(directory: string): Promise<void> {
     try {
-        await fs.rm(directory, { recursive: true, force: true });
+        await fsPromises.rm(directory, { recursive: true, force: true });
         console.log(`Cleaned up ${directory}`);
     } catch (error) {
         console.error(`Error cleaning up directory ${directory}:`, error);
